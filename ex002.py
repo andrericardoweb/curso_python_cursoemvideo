e75291a97fc3d8dd6c47898b527d172e393509a7
Andre Ricardo <andrericardoweb@gmail.com>
2021-04-30 11:50:18 -0300

    Exercício Python 002
    Exercício Python 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.

---
+++ ex002.py
@@ -0,0 +1,4 @@
+# Exercício Python 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+name = input('Qual o seu nome? ')
+welcome = 'Olá, ' + name + '! Seja bem vindo!'
+print(welcome)
