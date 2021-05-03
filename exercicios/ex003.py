30af22b9017d696baa9d09baae6de8cb8e4d60e2
Andre Ricardo <andrericardoweb@gmail.com>
2021-05-02 22:10:28 -0300

    Exercício Python #003​ - Somando dois números
    Exercício Python 003: Crie um programa que leia dois números e mostre a soma entre eles.

---
+++ exercicios/ex003.py
@@ -0,0 +1,5 @@
+# Exercício Python 003: Crie um programa que leia dois números e mostre a soma entre eles
+value1 = eval(input('Digite um valor: '))
+value2 = eval(input('Digite outro valor: '))
+result = value1 + value2
+print(f'A soma entre {value1} e {value2} é igual a {result}')
