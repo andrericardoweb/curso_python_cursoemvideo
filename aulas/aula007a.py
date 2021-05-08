364547b6d0f6296a5b6729dea7464eced24203f1
Andre Ricardo <andrericardoweb@gmail.com>
2021-05-08 10:56:17 -0300

    Aula 007 - Operadores Aritméticos

---
+++ aulas/aula007a.py
@@ -0,0 +1,9 @@
+# Aula 007 - Operadores Aritméticos
+n1 = eval(input('Um valor: '))
+n2 = eval(input('Outro valor: '))
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+exponenciacao = n1 ** n2
+print(f'Soma: {soma} \nSubtração: {subtracao} \nMultiplicação: {multiplicacao} \nDivisão: {divisao:.3f} \nExponenciação: {exponenciacao}')
